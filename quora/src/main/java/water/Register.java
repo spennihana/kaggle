ed6659ff328976a51a0e38b2e057dffb1ab82dc5
dev <dev@mbp.hsd1.ca.comcast.net>
2017-03-29 04:07:15 -0700

    use new method in handler

--- quora/src/main/java/water/Register.java
+++ quora/src/main/java/water/Register.java
@@ -5,6 +5,6 @@ import water.api.AbstractRegister;
 @SuppressWarnings("unused")
 public class Register extends AbstractRegister {
   @Override public void register(String relativeResourcePath) throws ClassNotFoundException {
-    H2O.register("GET /3/WordEm", WEHandler.class, "em", "", "Get word embeddings");
+    H2O.register("GET /3/WordEm", WEHandler.class, "em2", "", "Get word embeddings");
   }
 }
