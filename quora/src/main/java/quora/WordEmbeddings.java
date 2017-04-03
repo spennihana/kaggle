7d083e73eb2bc7e930cfe0fa34dd0ca887c5da7c
dev <dev@mbp.hsd1.ca.comcast.net>
2017-04-02 23:06:54 -0700

    time word embeddings hashmaps flattening

--- quora/src/main/java/quora/WordEmbeddings.java
+++ quora/src/main/java/quora/WordEmbeddings.java
@@ -7,11 +7,11 @@ import water.parser.BufferedString;
 import water.util.Log;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class WordEmbeddings {
 
-  private ParallelCsvRead.ParseBytesTask[] _pbTasks;
-  NonBlockingHashMap<BufferedString, double[]> _embeddings;
+  transient HashMap<BufferedString, double[]> _embeddings;
 
   public static WordEmbeddings read(String path) {
     WordEmbeddings em = new WordEmbeddings();
@@ -19,20 +19,23 @@ public class WordEmbeddings {
     long start = System.currentTimeMillis();
     long elapsed;
     long elapsed2;
+    long elapsed3;
     pcsv.raw_parse(); // suck in the raw bytes
     elapsed = System.currentTimeMillis() - start;
-    Log.info("Read raw bytes in to RAM in " + elapsed/1000. + " seconds" );
+    System.out.println("Disk to RAM read in " + elapsed/1000. + " seconds" );
     start = System.currentTimeMillis();
     pcsv.parse_bytes();
     elapsed2 = System.currentTimeMillis() - start;
-    Log.info("Parsed word embeddings in " + elapsed2/1000. + " seconds");
-    Log.info("Total embeddings parse time: " + (elapsed + elapsed2)/1000. +  " seconds");
-    em._embeddings = new NonBlockingHashMap<>();
+    System.out.println("Parsed word embeddings in " + elapsed2/1000. + " seconds");
+    em._embeddings = new HashMap<>();
+    start = System.currentTimeMillis();
     for(ParallelCsvRead.ParseBytesTask pbt: pcsv._pbtasks) {
       em._embeddings.putAll(pbt._rows);
       pbt._rows=null;
     }
-    em._pbTasks=null;
+    elapsed3 = System.currentTimeMillis() - start;
+    System.out.println("Reducing word embeddings together in " + elapsed3/1000. + " seconds");
+    System.out.println("Total embeddings parse time: " + (elapsed + elapsed2 + elapsed3)/1000. +  " seconds");
     return em;
   }
 
@@ -42,6 +45,6 @@ public class WordEmbeddings {
 
   public static void main(String[] args){
     WordEmbeddings wem = WordEmbeddings.read("./lib/w2vec_models/gw2vec");
-    System.out.println(Arrays.toString(wem.get("helo")));
+    System.out.println(Arrays.toString(wem.get("hello")));
   }
 }
