1ff7c6819c101aa1b7025cfc481358ef0ee84ec4
SpencerA <spnrpa@gmail.com>
2017-04-03 01:50:20 -0700

    load word embeddings from Quora main

--- quora/src/main/java/quora/Quora.java
+++ quora/src/main/java/quora/Quora.java
@@ -5,6 +5,7 @@ import water.H2O;
 public class Quora {
 
   public static void main(String[] args) {
+    WordEmbeddings em = new WordEmbeddings(); // load word embeddings statically now
     H2O.main(args);
   }
 
