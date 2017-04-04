8c88e26e349d62db7f7bd578a81fc85ae6c242f7
SpencerA <spnrpa@gmail.com>
2017-04-04 14:53:00 -0700

    add compressor method

--- quora/src/main/java/quora/WordEmbeddings.java
+++ quora/src/main/java/quora/WordEmbeddings.java
@@ -1,28 +1,31 @@
 package quora;
 
 
+import water.AutoBuffer;
 import water.Iced;
 import water.ParallelCsvRead;
 import water.parser.BufferedString;
 
+import java.io.*;
 import java.util.HashMap;
 
 public class WordEmbeddings extends Iced {
 
   public enum WORD_EM {
     GLOVE(_embeddings_glove),
-    GOOGL(_embeddings_googl);
+    GOOGL(null);
     private final HashMap<BufferedString,double[]> _em;
     WORD_EM(HashMap<BufferedString,double[]> em) { _em=em; }
     public double[] get(String w) { return _em.get(new BufferedString(w)); }
   }
-  public static transient HashMap<BufferedString, double[]> _embeddings_googl;
+//  public static transient HashMap<BufferedString, double[]> _embeddings_googl;
   public static transient HashMap<BufferedString, double[]> _embeddings_glove;
 
   static {
     // parse word embeddigns statically
-    _embeddings_googl = read("./lib/w2vec_models/gw2vec",false);
+//    _embeddings_googl = read("./lib/w2vec_models/gw2vec",false);
     _embeddings_glove = read("./lib/w2vec_models/glove.840B.300d.txt",true);
+    compress();
   }
 
   public static HashMap<BufferedString, double[]> read(String path, boolean glove) {
@@ -49,5 +52,31 @@ public class WordEmbeddings extends Iced {
     System.out.println("Reducing word embeddings together in " + elapsed3/1000. + " seconds");
     System.out.println("Total embeddings parse time: " + (elapsed + elapsed2 + elapsed3)/1000. +  " seconds");
     return embeddings;
+  }
+
+
+  static void compress() {
+    long s = System.currentTimeMillis();
+    System.out.println("compressing embeddings...");
+    HashMap<BufferedString,double[]> em = _embeddings_glove;
+    String outpath = "./lib/w2vec_models/glove.bin";
+    try(FileOutputStream out = new FileOutputStream(new File(outpath))) {
+      for(BufferedString bs: em.keySet()) {
+        AutoBuffer ab = new AutoBuffer();
+        byte[] strbits = bs.getBuffer();
+        ab.put4(strbits.length);
+        ab.putA1(strbits);
+        double[] d = em.get(bs);
+        for(double dd: d) ab.put8d(dd);
+        out.write(ab.buf());
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    System.out.println("Finished in " + (System.currentTimeMillis() - s)/1000. + " seconds");
+  }
+
+  public static void main(String[] args) {
+    System.out.println("yayyy");
   }
 }
